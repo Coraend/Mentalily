840a54d37fb0962b2b0469b58bf4251bf36eb946
Coraend <coraendzz@gmail.com>
2025-02-12 17:10:27 +0700

    Add files via upload

---
+++ jsindex.js
@@ -0,0 +1,15 @@
+const item3 = document.querySelector('#item3');
+
+function cursorOver() {
+    item3.style.background = '#878787';
+}
+
+function cursorOut() {
+    item3.style.background = '#629584';
+}
+
+const buttonMore = document.querySelector('#buttonMore');
+
+function moreMouseClick() {
+    
+}
